38c9452a4a3c49e4a0e0246c8ab26ab8db01be2e
Кислый Кислюк <kislball@yahoo.com>
2021-02-15 11:12:36 +0300

    org(lint): smol fix

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'type-enum': [2, '', [
+    'type-enum': [2, 'always', [
       'feat',
       'fix',
       'perf',
