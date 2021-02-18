0c64ac20e9a9024681a6c9fb85fae147ef1c9d54
кислый кислюк <kislball@yahoo.com>
2021-02-18 20:19:22 +0300

    halp

--- packages/discordoo/src/index.ts
+++ packages/discordoo/src/index.ts
@@ -1,11 +1,12 @@
 import createPreloadedClient from './createPreloadedClient'
 import { CacheModule } from '@discordoo/cache'
 
-const Client = createPreloadedClient(new CacheModule())
-
-export { createPreloadedClient, Client }
-
 export * as Cache from '@discordoo/cache'
 export * as Core from '@discordoo/core'
 export * as Collection from '@discordoo/collection'
 export * as WS from '@discordoo/ws'
+
+const Client = createPreloadedClient(new CacheModule())
+
+export { createPreloadedClient, Client }
+
