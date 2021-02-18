99de2abc498bced3c73d78b5e00e3cc4e32d7390
кислый кислюк <kislball@yahoo.com>
2021-02-18 20:22:17 +0300

    org(docs): final change, i guess

--- packages/discordoo/src/index.ts
+++ packages/discordoo/src/index.ts
@@ -1,12 +1,13 @@
 import createPreloadedClient from './createPreloadedClient'
 import { CacheModule } from '@discordoo/cache'
+import { Client as OriginalClient } from '@discordoo/core'
 
 export * as Cache from '@discordoo/cache'
 export * as Core from '@discordoo/core'
 export * as Collection from '@discordoo/collection'
 export * as WS from '@discordoo/ws'
 
-const Client = createPreloadedClient(new CacheModule())
+const Client: typeof OriginalClient = createPreloadedClient(new CacheModule())
 
 export { createPreloadedClient, Client }
 
