ad5d8feb7ebde9504525a72bc93ae0a1634f9435
кислый кислюк <kislball@yahoo.com>
2021-02-18 19:46:59 +0300

    feat(discordoo): exporting all modules from core package

--- packages/discordoo/src/index.ts
+++ packages/discordoo/src/index.ts
@@ -4,3 +4,8 @@ import { CacheModule } from '@discordoo/cache'
 const Client = createPreloadedClient(new CacheModule())
 
 export { createPreloadedClient, Client }
+
+export * as Cache from '@discordoo/cache'
+export * as Core from '@discordoo/core'
+export * as Collection from '@discordoo/collection'
+export * as WS from '@discordoo/ws'
