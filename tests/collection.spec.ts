a4e83ce2a5195e59afcd28e5c2e2d18edf20fd7f
кислый кислюк <kislball@yahoo.com>
2021-02-18 09:02:53 +0300

    org(tests): adding unit tests

---
+++ tests/collection.spec.ts
@@ -0,0 +1,27 @@
+import Collection from '../packages/collection'
+
+describe('Collection', () => {
+  test('clone', () => {
+    const testCol = new Collection<string, number>()
+    testCol.set('abc', 5)
+    testCol.set('abcd', 6)
+    expect(testCol.equal(testCol.clone())).toBe(true)
+  })
+
+  test('equal', () => {
+    const testCol = new Collection<string, any>()
+    const testCol2 = new Collection<string, any>()
+    testCol.set('5', 10)
+    testCol2.set('5', 10)
+
+    expect(testCol.equal(testCol2)).toBe(true)
+
+    testCol2.set('5', 345)
+    expect(testCol.equal(testCol2)).toBe(false)
+  })
+
+  test('filter', () => {
+    const testCol = new Collection<string, any>([['test', 5], ['test3', 10], ['test4', 2], ['test5', 1]])
+    expect(testCol.filter(i => i >= 5).equal(new Collection<string, any>([['test', 5], ['test3', 10]]))).toBe(true)
+  })
+})
