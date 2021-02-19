7ff3528ca4f9504167a4f7ee56d1af3d32f610bb
кислый кислюк <kislball@yahoo.com>
2021-02-19 18:44:42 +0300

    feat(core): adding get*Cache for better aliases

--- packages/core/src/modules/cache/CacheModule.ts
+++ packages/core/src/modules/cache/CacheModule.ts
@@ -1,5 +1,14 @@
 import CoreModule from '../CoreModule'
 import CacheCell from './CacheCell'
+import {
+  APIEmoji,
+  APIChannel,
+  APIGuild,
+  APIGuildMember,
+  APIUser,
+  GatewayActivity,
+  APIMessage
+} from 'discord-api-types'
 
 /**
  * Represents a single cache module
@@ -9,4 +18,11 @@ import CacheCell from './CacheCell'
 export default interface CacheModule extends CoreModule {
   createCache<K extends CacheCell<any, any>>(type: string): K
   getCache<K extends CacheCell<any, any>>(type: string): K | undefined
+  getChannelCache(guild?: string): CacheCell<string, APIChannel>
+  getGuildEmojiCache(guild: string): CacheCell<string, APIEmoji>
+  getGuildCache(): CacheCell<string, APIGuild>
+  getMemberCache(guild: string): CacheCell<string, APIGuildMember>
+  getUserCache(): CacheCell<string, APIUser>
+  getPresencesCache(): CacheCell<string, GatewayActivity>
+  getMessagesCache(channel: string): CacheCell<string, APIMessage>
 }
