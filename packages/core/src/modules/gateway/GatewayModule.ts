221218e7f76fc62c28dddccefe902ade9a74e00c
кислый кислюк <kislball@yahoo.com>
2021-02-21 18:46:47 +0300

    fix(core): adding connection generic when necessary

--- packages/core/src/modules/gateway/GatewayModule.ts
+++ packages/core/src/modules/gateway/GatewayModule.ts
@@ -2,7 +2,7 @@ import CoreModule from '../CoreModule'
 import GatewayModuleConfig from './GatewayModuleConfig'
 import ShardsManager from './ShardsManager'
 
-export default interface GatewayModule extends CoreModule {
+export default interface GatewayModule<C> extends CoreModule {
   config: GatewayModuleConfig
-  shards: ShardsManager
+  shards: ShardsManager<C>
 }
