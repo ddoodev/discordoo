a941e4d7ec182a210ff03be96fafeae45c172d51
кислый кислюк <kislball@yahoo.com>
2021-02-25 20:06:21 +0300

    feat(core): making all fields optional

--- packages/core/src/modules/gateway/GatewayModuleConfig.ts
+++ packages/core/src/modules/gateway/GatewayModuleConfig.ts
@@ -5,18 +5,18 @@ export default interface GatewayModuleConfig {
   /**
    * Token, that will be passed to shards
    */
-  token: string,
+  token?: string,
   /**
    * File, which shall be clustered.
    *
    * Should be always default to __filename
    */
-  file: string
+  file?: string
   /**
    * Shards for this module
    *
    * Double array is for range of shards, array of numbers is for specific shard ids, just number is for total amount of
    * shards
    */
-  shards: [number, number] | number[] | number
+  shards?: [number, number] | number[] | number
 }
