d9c7555f56de542f0bfeed7ad7c505c4ee90546d
Кислый Кислюк <kislball@yahoo.com>
2021-03-21 18:11:22 +0300

    feat(collection): collection implementation

---
+++ src/collection/index.ts
@@ -0,0 +1,40 @@
+import isEqual from 'lodash/isEqual'
+
+export default class Collection<K, V> extends Map<K, V> {
+  random(amount?: number): V[] | V {
+    const getRandomElement = () => this.get([ ...this.keys() ][Math.floor(Math.random() * this.size)])
+    if (amount === undefined) {
+      return getRandomElement()!
+    } else {
+      if (amount > this.size) throw new RangeError('Cannot get more random elements that are in the Collection')
+      else if (amount === this.size) return [ ...this.values() ]
+      const result = []
+
+      // eslint-disable-next-line
+      for (const i of Array(amount).fill(228)) {
+        result.push(getRandomElement()!)
+      }
+      return result
+    }
+  }
+
+  filter(filter: (value: V, key: K, collection: Collection<K, V>) => boolean): Collection<K, V> {
+    const entries = [ ...this.entries() ]
+
+    return new Collection<K, V>(entries.filter((v) => filter(v[1], v[0], this)))
+  }
+
+  clone(): Collection<K, V> {
+    return new Collection<K, V>([ ...this ])
+  }
+
+  equal(collection: Collection<K, V>): boolean { // looks like O(n), if it isn't, refactor
+    if (this.size !== collection.size) return false
+    for (const [ key, val ] of this) {
+      if (!collection.has(key)) return false
+      const testValue = collection.get(key)!
+      if (!isEqual(val, testValue)) return false
+    }
+    return true
+  }
+}
