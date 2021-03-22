af1427abb30e9010ff179791fbcc020d327d2a80
Кислый Кислюк <kislball@yahoo.com>
2021-03-22 15:23:40 +0300

    feat(client): destroyed module hook

--- src/core/modules/Module.ts
+++ src/core/modules/Module.ts
@@ -3,5 +3,6 @@ import Client from '../Client'
 export default interface Module {
   initialized: boolean
   init?: (client: Client) => Promise<void> | void
+  destroyed?: () => Promise<void> | void
   id: string | symbol
 }
