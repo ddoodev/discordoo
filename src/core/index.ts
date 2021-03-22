dad6798fb36eed4bc643277cecd25544377e8431
Кислый Кислюк <kislball@yahoo.com>
2021-03-22 15:35:18 +0300

    fix(client): adding module-related exports

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,7 +1,11 @@
 import Client from './Client'
 import ClientEventHandlers from './ClientEventHandlers'
+import Module from './modules/Module'
+import ModuleManager from './modules/ModuleManager'
 
 export {
   Client,
-  ClientEventHandlers
+  ClientEventHandlers,
+  Module,
+  ModuleManager
 }
