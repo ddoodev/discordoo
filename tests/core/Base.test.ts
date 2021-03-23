f50aee8c3a102b86277702f2dffabb8fe16af8dc
Кислый Кислюк <kislball@yahoo.com>
2021-03-23 20:24:42 +0300

    test(core): base

--- tests/core/Base.test.ts
+++ tests/core/Base.test.ts
@@ -5,4 +5,18 @@ describe('Base', () => {
     const abc = new Base('419524085736013834')
     expect(abc.createdAt.getTime()).toBe(new Date(1520092736420).getTime())
   })
+
+  describe('equal', () => {
+    const abc = new Base('419524085736013834')
+    const def = new Base('419524085736013834')
+    const idk = new Base('405044179182419980')
+
+    test('not equal', () => {
+      expect(abc.equal(idk)).toBe(false)
+    })
+
+    test('equal', () => {
+      expect(abc.equal(def)).toBe(true)
+    })
+  })
 })
