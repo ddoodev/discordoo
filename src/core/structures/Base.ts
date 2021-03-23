fa5aec0d87083d9654a55c22ab164f0acc6797d6
Кислый Кислюк <kislball@yahoo.com>
2021-03-23 20:21:19 +0300

    docs(core): base

--- src/core/structures/Base.ts
+++ src/core/structures/Base.ts
@@ -1,23 +1,40 @@
+import idToDate from '../../util/idToDate'
+
 /**
  * Base structure for everything which has snowflake
  */
-import idToDate from '../../util/idToDate'
-
 export default class Base {
+  /**
+   * Unique id
+   */
   id: string
 
+  /**
+   * @param id - id
+   */
   constructor(id: string) {
     this.id = id
   }
 
+  /**
+   * Check, if two entities are equal
+   *
+   * @param compareTo - entity to compare to
+   */
   equal(compareTo: Base) {
     return compareTo.id === this.id
   }
 
+  /**
+   * Get creation date
+   */
   get createdAt() {
     return idToDate(this.id)
   }
 
+  /**
+   * Unix timestamp
+   */
   get createdTimestamp() {
     return this.createdAt.getDate()
   }
