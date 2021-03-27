e040d45ccfa88bf6c3c94fceadd5b58ce4eb3154
Кислый Кислюк <kislball@yahoo.com>
2021-03-27 23:46:46 +0300

    ci(docgen): setting output to markdown

--- typedoc.js
+++ typedoc.js
@@ -3,6 +3,5 @@ const { packages } = require('./discordoo.json')
 
 module.exports = {
   entryPoints: packages.map(e => `./src/${e}/index.ts`),
-  json: './docs/docs.json',
-  readme: './README.md'
+  readme: 'none'
 }
