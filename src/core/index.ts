11a6501aa356090d8a461df7bde19ac870417b17
Кислый Кислюк <kislball@yahoo.com>
2021-03-28 10:02:37 +0300

    fix(core): adding exports for rest interfaces

--- src/core/index.ts
+++ src/core/index.ts
@@ -2,10 +2,18 @@ import Client from './Client'
 import ClientEventHandlers from './ClientEventHandlers'
 import Module from './modules/Module'
 import ModuleManager from './modules/ModuleManager'
+import RequestBuilder from './modules/rest/RequestBuilder'
+import RequestOptions from './modules/rest/RequestOptions'
+import RestModule from './modules/rest/RestModule'
+import RestModuleEvents from './modules/rest/RestModuleEvents'
 
 export {
   Client,
   ClientEventHandlers,
   Module,
-  ModuleManager
+  ModuleManager,
+  RequestBuilder,
+  RequestOptions,
+  RestModule,
+  RestModuleEvents
 }
