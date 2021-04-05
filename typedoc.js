2eb84b2665527058e08b7989b59eec543bd83a58
Mirdukkk <vdvmirduk@gmail.com>
2021-04-05 23:13:50 +0300

    chore: change typedoc configuration

--- typedoc.js
+++ typedoc.js
@@ -4,5 +4,6 @@ const { packages } = require('./discordoo.json')
 module.exports = {
   entryPoints: packages.map(e => `./src/${e}/index.ts`),
   readme: 'none',
+  excludeExternals: true,
   json: './docs/docs.json'
 }
