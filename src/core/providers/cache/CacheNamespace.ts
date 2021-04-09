9db06a979f91c8ddbf0622d358896507e54a95a4
Кислый Кислюк <kislball@yahoo.com>
2021-04-09 20:42:10 +0300

    docs(core): add note about reason of using promises

--- src/core/providers/cache/CacheNamespace.ts
+++ src/core/providers/cache/CacheNamespace.ts
@@ -1,4 +1,7 @@
-/** Represents a cache namespace. Custom caching modules must implements it */
+/**
+ * Represents a cache namespace. Custom caching modules must implement it
+ * @note Some implementations may use remote cache, e.g. Redis, so all the methods are async
+ */
 export default interface CacheNamespace<K = unknown, V = unknown> {
   get(key: K): Promise<V>
   set(key: K, value: V): Promise<boolean>
