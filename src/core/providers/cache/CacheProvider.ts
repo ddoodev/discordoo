f3826b853774f3861021523fc1793295107ca872
Кислый Кислюк <kislball@yahoo.com>
2021-04-09 20:33:52 +0300

    fix(core): remove redundant generic type

--- src/core/providers/cache/CacheProvider.ts
+++ src/core/providers/cache/CacheProvider.ts
@@ -1,6 +1,6 @@
 import CacheNamespace from './CacheNamespace'
 
 /** Represents a CacheProvider. Bound to Client context */
-type CacheProvider<T extends CacheNamespace> = <K, V>(namespace: string) => T | Promise<T>
+type CacheProvider<T extends CacheNamespace> = (namespace: string) => T | Promise<T>
 
 export default CacheProvider
