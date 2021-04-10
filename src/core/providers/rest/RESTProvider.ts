b014486abb302d0a4e4c0a4c58e50a03cb846ac3
Mirdukkk <vdvmirduk@gmail.com>
2021-04-10 17:57:49 +0300

    fix(rest): add default to rest provider generic

--- src/core/providers/rest/RESTProvider.ts
+++ src/core/providers/rest/RESTProvider.ts
@@ -1,6 +1,6 @@
 import RequestBuilder from './RequestBuilder'
 
 /** Represents a RESTProvider. Bound to Client context */
-type RESTProvider<T extends RequestBuilder> = () => T
+type RESTProvider<T extends RequestBuilder = RequestBuilder> = () => T
 
 export default RESTProvider
