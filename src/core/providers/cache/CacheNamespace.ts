4541597102854e1a7849739498b863d3969fad95
Кислый Кислюк <kislball@yahoo.com>
2021-04-10 20:35:27 +0300

    fix(core): add support for null return value in CollectionNamespace#get

--- src/core/providers/cache/CacheNamespace.ts
+++ src/core/providers/cache/CacheNamespace.ts
@@ -3,7 +3,7 @@
  * @note Some implementations may use remote cache, e.g. Redis, so all the methods are async
  */
 export default interface CacheNamespace<K = unknown, V = unknown> {
-  get(key: K): Promise<V>
+  get(key: K): Promise<V | null>
   set(key: K, value: V): Promise<boolean>
   delete(key: K): Promise<boolean>
   has(key: K): Promise<boolean>
