a513fbf44e26c44f33686b58fc29a1aa36f6ce4b
Mirdukkk <vdvmirduk@gmail.com>
2021-04-10 17:49:10 +0300

    feat(cache): add size to CacheNamespace

--- src/core/providers/cache/CacheNamespace.ts
+++ src/core/providers/cache/CacheNamespace.ts
@@ -7,4 +7,5 @@ export default interface CacheNamespace<K = unknown, V = unknown> {
   set(key: K, value: V): Promise<boolean>
   delete(key: K): Promise<boolean>
   has(key: K): Promise<boolean>
+  size(): Promise<number>
 }
