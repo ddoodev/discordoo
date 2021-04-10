6c2afebaa015679207aaa6fef06f60bfc5c809a9
Mirdukkk <vdvmirduk@gmail.com>
2021-04-10 17:51:38 +0300

    chore: rename ClientEventHandlers to ClientEventsHandlers

--- src/core/ClientEventHandlers.ts
+++
@@ -1,5 +0,0 @@
-/** Client events */
-export default interface ClientEventHandlers {
-  /** Emitted once the client is ready */
-  ready: () => void,
-}
---
+++ src/core/ClientEventsHandlers.ts
@@ -0,0 +1,7 @@
+/** Client events */
+export default interface ClientEventsHandlers {
+
+  /** Emitted once the client is ready */
+  ready: () => unknown
+
+}
