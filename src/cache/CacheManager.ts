4674429f3c8eda8cd416ba4fcceb0affce77405f
Кислый Кислюк <kislball@yahoo.com>
2021-04-13 20:55:28 +0300

    docs(cache/core): document cache-related stuff

--- src/cache/CacheManager.ts
+++ src/cache/CacheManager.ts
@@ -1,9 +1,19 @@
 import { Collection } from '@src/collection'
 import CollectionCacheNamespace from '@src/cache/CollectionCacheNamespace'
 
+/**
+ * Represents a cache manager
+ *
+ * Used for cache namespaces fetching
+ * */
 export default class CacheManager {
+  /** Namespaces controlled by this manager */
   caches: Collection<string, CollectionCacheNamespace> = new Collection
 
+  /**
+   * Get a cache namespace
+   * @param id - id of cache namespace
+   */
   getCache(id: string): CollectionCacheNamespace {
     if (this.caches.has(id)) return this.caches.get(id)!
     else {
--- src/cache/CacheProviderBuilder.ts
+++ src/cache/CacheProviderBuilder.ts
@@ -1,9 +1,12 @@
 import { CacheProvider, Client } from '@src/core'
 import CacheManager from '@src/cache/CacheManager'
 
+/** An adapter of {@link CacheManager} for {@link Client} */
 export default class CacheProviderBuilder {
+  /** Cache manager, adapted by this adapter */
   manager: CacheManager = new CacheManager
 
+  /** Get a cache provider */
   getCacheProvider(): (client: Client) => CacheProvider {
     const manager = this.manager // this.manager is shadowed in nested function
 
