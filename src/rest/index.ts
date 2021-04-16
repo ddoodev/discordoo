ab52aaba919918ea68ce1888a266b4c38ff56860
Кислый Кислюк <kislball@yahoo.com>
2021-04-16 23:25:53 +0300

    feat(rest): export RESTClient

--- src/rest/index.ts
+++ src/rest/index.ts
@@ -1,8 +1,10 @@
+import RESTClient from '@src/rest/RESTClient'
 import RESTOptions from '@src/rest/RESTOptions'
 import RESTProviderBuilder from '@src/rest/RESTProviderBuilder'
 import RESTRequestBuilder from '@src/rest/RESTRequestBuilder'
 
 export {
+  RESTClient,
   RESTOptions,
   RESTProviderBuilder,
   RESTRequestBuilder
