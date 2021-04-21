1646e054fec0b1a942340584d4bdf638a28dd221
Кислый Кислюк <kislball@yahoo.com>
2021-04-21 19:33:46 +0300

    feat(util): add getRecommendedShards function

---
+++ src/util/getRecommendedShards.ts
@@ -0,0 +1,8 @@
+import { RESTRequestBuilder } from '@src/rest'
+import { Constants } from '@src/core'
+import { RESTGetAPIGatewayBotResult } from 'discord-api-types'
+
+export default async function getRecommendedGateway(token: string): Promise<RESTGetAPIGatewayBotResult> {
+  const req = new RESTRequestBuilder(token, Constants.DEFAULT_REST_OPTIONS).url('gateway', 'bot')
+  return (await req.get<RESTGetAPIGatewayBotResult>({})).body
+}
