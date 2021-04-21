ba5fdb4e5371741dd275ca09cc4cea204066c24c
Кислый Кислюк <kislball@yahoo.com>
2021-04-21 19:35:04 +0300

    feat(util): rename getRecommendedGateway to getGateway

--- src/util/getGateway.ts
+++ src/util/getGateway.ts
@@ -2,7 +2,7 @@ import { RESTRequestBuilder } from '@src/rest'
 import { Constants } from '@src/core'
 import { RESTGetAPIGatewayBotResult } from 'discord-api-types'
 
-export default async function getRecommendedGateway(token: string): Promise<RESTGetAPIGatewayBotResult> {
+export default async function getGateway(token: string): Promise<RESTGetAPIGatewayBotResult> {
   const req = new RESTRequestBuilder(token, Constants.DEFAULT_REST_OPTIONS).url('gateway', 'bot')
   return (await req.get<RESTGetAPIGatewayBotResult>({})).body
 }
