a65a2c0e0e851d00df9a23dad750ae2604ae9910
TheLionCub <53406744+TheLionCub@users.noreply.github.com>
2021-06-20 23:48:53 +0300

    feat(collection): add some useful methods & fixes
    Rewritten random() method, now it works 1.4 times faster

--- src/collection/Collection.ts
+++ src/collection/Collection.ts
@@ -3,7 +3,7 @@ import CollectionEqualOptions from '@src/collection/interfaces/CollectionEqualOp
 import CollectionFilterOptions from '@src/collection/interfaces/CollectionFilterOptions'
 import CollectionRandomOptions from '@src/collection/interfaces/CollectionRandomOptions'
 
-let lodashIsEqual
+let lodashIsEqual: equalFn
 
 try {
   lodashIsEqual = require('lodash/isEqual')
@@ -11,42 +11,59 @@ try {
 
 /** An utility data structure used within the library */
 export default class Collection<K = unknown, V = unknown> extends Map<K, V> {
-
-  /** Get a random element from collection (can return several identical results if the amount is specified) */
+  /**
+   * Gets a random element from collection (can return several identical results if the amount is specified).
+   * */
   random(): V
   random(amount: number, options?: CollectionRandomOptions): V[]
   random(amount?: number, options?: CollectionRandomOptions): V | V[]
   random(amount?: number, options: CollectionRandomOptions = {}): V | V[] {
     const size = this.size
 
-    if (size < 1) throw new DiscordooError('Collection#random', 'Cannot get random elements from the empty collection')
-    if (amount && amount > size) amount = size
-    if (!amount || (amount && amount < 1)) amount = 1
-    if (typeof options.unique !== 'boolean') options.unique = !!options.unique
+    if (size < 1) throw new RangeError('Cannot get random elements in the empty collection')
 
-    const random: number[] = []
+    if (amount) {
+      if (amount < 1) {
+        throw new RangeError('Cannot get less than 1 element from collection')
+      }
 
-    for (let i = 0; i < amount; i++) {
-      switch (true) {
-        case options.unique: {
-          const num = Math.floor(Math.random() * size)
-          random.indexOf(num) > -1 ? i -= 1 : random.push(num) // repeat iteration if number is not unique
-        } break
+      if (amount > size) {
+        amount = size
+      }
+    } else {
+      return this.getValues()[Math.floor(Math.random() * size)]
+    }
 
-        default:
-          random.push(Math.floor(Math.random() * size))
-          break
+    if (amount === 1) {
+      return this.getValues()[Math.floor(Math.random() * size)]
+    }
+
+    const result: V[] = []
+    const values: V[] = this.getValues()
+
+    if (options.unique) {
+      const random: number[] = []
+
+      for (let i = 0; i < amount; i++) {
+        const num: number = Math.floor(Math.random() * size)
+        if (random.indexOf(num) > -1) {
+          i -= 1
+        } else {
+          random.push(num)
+          result.push(values[num])
+        }
+      }
+    } else {
+      for (let i = 0; i < amount; i++) {
+        result.push(values[Math.floor(Math.random() * size)])
       }
     }
 
-    const arr = [ ...this.values() ]
-    const results: V[] = random.map(r => arr[r])
-
-    return amount <= 1 ? results[0] : results
+    return result
   }
 
   /**
-   * Filter out the elements which don't meet requirements
+   * Filters out the elements which don't meet requirements.
    */
   filter<T>(
     filter: (value: V, key: K, collection: Collection<K, V>) => unknown,
@@ -59,18 +76,15 @@ export default class Collection<K = unknown, V = unknown> extends Map<K, V> {
     let results, predicate
 
     switch (options.return) {
-      case 'array':
-        results = []
-        predicate = (v, k, c) => filter(v, k, c) && results.push([ k, v ])
-        break
       case 'map':
-        results = new Map<K, V>()
+        results = new Map()
         predicate = (v, k, c) => filter(v, k, c) && results.set(k, v)
         break
       case 'collection':
         results = new Collection<K, V>()
         predicate = (v, k, c) => filter(v, k, c) && results.set(k, v)
         break
+      case 'array':
       default:
         results = []
         predicate = (v, k, c) => filter(v, k, c) && results.push([ k, v ])
@@ -85,7 +99,7 @@ export default class Collection<K = unknown, V = unknown> extends Map<K, V> {
   }
 
   /**
-   * Execute a function on each of elements of map
+   * Executes a function on each of the elements of the map.
    * @param predicate - function to use
    */
   forEach(predicate: (value: V, key: K, collection: Collection<K, V>) => unknown) {
@@ -94,13 +108,15 @@ export default class Collection<K = unknown, V = unknown> extends Map<K, V> {
     })
   }
 
-  /** Create a new collection based on this one */
+  /**
+   *  Creates a new collection based on this one.
+   *  */
   clone(): Collection<K, V> {
     return new Collection<K, V>([ ...this ])
   }
 
   /**
-   * Check if two collections are equal
+   * Checks if two collections are equal.
    * @param {Collection} collection - collection to compare to
    * @param {CollectionEqualOptions} options - options to use
    */
@@ -108,20 +124,131 @@ export default class Collection<K = unknown, V = unknown> extends Map<K, V> {
     if (this.size !== collection?.size) return false
     if (this === collection) return true
 
-    let equal = (arg1: any, arg2: any) => arg1 === arg2
-
     if (options.deep) {
       if (!lodashIsEqual) throw new DiscordooError('Collection#equal', 'cannot perform deep equal without lodash installed')
-      else equal = (arg1: any, arg2: any) => lodashIsEqual(arg1, arg2)
     }
 
-    for (const [ key, value ] of this) {
+    for (const [ key, value ] of this.entries()) {
       switch (true) { // switch is faster than if, so we use it in the loop
-        case !collection.has(key) || !equal(collection.get(key), value):
+        case !collection.has(key) || !lodashIsEqual(collection.get(key), value):
           return false
       }
     }
 
     return true
   }
+
+  /*
+  * Checks if collection is empty.
+  * */
+  isEmpty(): boolean {
+    return this.size === 0;
+  }
+
+  /**
+   * Checks if collection has specified value.
+   * @param value - value to compare with
+   * */
+  hasValue(value: V): boolean {
+    for (const [k, v] of this.entries()) {
+      if (v === value) {
+        return true
+      }
+    }
+
+    return false
+  }
+
+  /**
+   * Returns the value to which the specified key is mapped,
+   * or defaultValue if this map contains no mapping for the key.
+   * @param key - key to get the value of
+   * @param defaultValue - value to return if key value does not exist
+   * */
+  getOrDefault(key: K, defaultValue: V): V {
+    return this.get(key) || defaultValue
+  }
+
+  /*
+  * If the specified key is not already associated with a value (or is mapped to null)
+  * associates it with the given value and returns null, else returns the current value.
+  * */
+  setIfAbsent(key: K, value: V): V | null {
+    if (this.has(key) && !this.get(key)) {
+      this.set(key, value)
+      return value
+    }
+
+    return null
+  }
+
+  /*
+  * Copies all of the mappings from the specified map / collection to this collection.
+  * */
+  setAll<K1 extends K, V1 extends V>(map: Map<K1, V1> | Collection<K1, V1>): void {
+    for (const [key, value] of map) {
+      this.set(key, value)
+    }
+  }
+
+  /*
+  * Returns a set of keys contained in this map.
+  * */
+  getKeySet(): Set<K> {
+    return new Set<K>([...this.keys()])
+  }
+
+  /*
+  * Returns a set of collection values.
+  * */
+  getValueSet(): Set<V> {
+    return new Set<V>([...this.values()])
+  }
+
+  /*
+  * Returns a set of collection entries.
+  * */
+  getEntrySet(): Set<[K, V]> {
+    return new Set<[K, V]>([...this.entries()])
+  }
+
+  /*
+  * Returns an array of collection keys.
+  * */
+  getKeys(): K[] {
+    return [...this.keys()]
+  }
+
+  /*
+  * Returns an array of collection values.
+  * */
+  getValues(): V[] {
+    return [...this.values()]
+  }
+
+  /*
+  * Returns an array of collection entries.
+  * */
+  getEntries(): [K, V][] {
+    return [...this.entries()]
+  }
+
+  /*
+  * Returns merged collection.
+  * */
+  merge(...collections: Collection<K, V>[]): Collection<K, V> {
+    const merged = this
+
+    for (const collection of collections) {
+      if (!collection) {
+        continue
+      }
+
+      merged.setAll(collection)
+    }
+
+    return merged
+  }
 }
+
+type equalFn = (arg1: any, arg2: any) => boolean
