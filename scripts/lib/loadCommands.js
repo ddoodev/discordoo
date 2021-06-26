dc02f87acd8f99855e8d0b2a5ffea5b98681d527
Mirdukkk <vdvmirduk@gmail.com>
2021-06-26 22:59:36 +0300

    chore(lint): configure .eslintignore
    chore: remove useless scripts

--- scripts/lib/loadCommands.js
+++
@@ -1,7 +0,0 @@
-// eslint-disable-next-line
-const [ fs, path ] = [ 'fs', 'path' ].map(require)
-
-module.exports = () => {
-  const paths = fs.readdirSync(path.resolve('./scripts/cmds')).map(e => path.resolve(`./scripts/cmds/${e}`))
-  return paths.map(require)
-}
