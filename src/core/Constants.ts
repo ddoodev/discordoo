636543a18d247ec276b9f5d1fabe000fd0983a95
Mirdukkk <vdvmirduk@gmail.com>
2021-07-05 22:01:28 +0300

    feat: add some comments

--- src/core/Constants.ts
+++ src/core/Constants.ts
@@ -5,104 +5,176 @@ import { GatewayOptions } from '@src/gateway'
 
 export const RAW_IPC_EVENT = '__DDOO__.MESSAGE'
 
+/** Global caching policy used to override any other caching policies */
 export enum GlobalCachingPolicy {
+  /** Cache everything we can */
   ALL = 'all',
+  /** Cache absolutely nothing */
   NONE = 'none',
 }
 
+/** Messages caching policy */
 export enum MessagesCachingPolicy {
+  /** Cache all messages */
   ALL = 'all',
+  /** Dont cache messages */
   NONE = 'none',
+  /** Cache messages from users */
   USERS = 'users',
+  /** Cache messages from bots */
   BOTS = 'bots',
 }
 
+/** Guilds caching policy */
 export enum GuildsCachingPolicy {
+  /** Cache all guilds */
   ALL = 'all',
+  /** Dont cache guilds */
   NONE = 'none',
 }
 
+/** Members caching policy */
 export enum MembersCachingPolicy {
+  /** Cache all members which library received */
   ALL = 'all',
+  /** Dont cache members */
   NONE = 'none',
+  /** Cache online members */
   ONLINE = 'online',
+  /** Cache members with dnd */
   DND = 'dnd',
+  /** Cache with idle status */
   IDLE = 'idle',
+  /** Cache offline members */
   OFFLINE = 'offline',
+  /** Cache guilds owners */
   OWNER = 'owner',
+  /** Cache members which are pending due to discord's welcome screen */
   PENDING = 'pending',
+  /** Cache connected to voice channels members */
   VOICE = 'voice',
+  /** Cache members who recently send a message */
   RECENT_MESSAGE = 'recentMessage',
 }
 
+/** Channels caching policy */
 export enum ChannelsCachingPolicy {
+  /** Cache all channels */
   ALL = 'all',
+  /** Dont cache channels */
   NONE = 'none',
+  /** Cache text channels */
   TEXT = 'text',
+  /** Cache voice channels (does not includes stages) */
   VOICE = 'voice',
+  /** Cache dm channels */
   DM = 'dm',
+  /** Cache categories */
   CATEGORY = 'category',
+  /** Cache news channels (does not includes threads) */
   NEWS = 'news',
+  /** Cache 'store' channels (does not includes threads) */
   STORE = 'store',
+  /** Cache news threads */
   NEWS_THREAD = 'newsThread',
+  /** Cache public threads */
   PUBLIC_THREAD = 'publicThread',
+  /** Cache private threads */
   PRIVATE_THREAD = 'privateThread',
+  /** Cache stage voices */
   STAGE_VOICE = 'stageVoice',
 }
 
+/** Emojis caching policy */
 export enum EmojisCachingPolicy {
+  /** Cache all emojis */
   ALL = 'all',
+  /** Dont cache emojis */
   NONE = 'none',
+  /** Cache animated emojis */
   ANIMATED = 'animated',
+  /** Cache static emojis (common) */
   STATIC = 'static',
 }
 
+/** Roles caching policy */
 export enum RolesCachingPolicy {
+  /** Cache all roles */
   ALL = 'all',
+  /** Dont cache roles */
   NONE = 'none',
+  /** Cache @everyone roles */
   EVERYONE = 'everyone',
+  /** Cache roles that managed by integrations */
   MANAGED = 'managed',
 }
 
+/** Presences caching policy */
 export enum PresencesCachingPolicy {
+  /** Cache all presences */
   ALL = 'all',
+  /** Dont cache presences */
   NONE = 'none',
 }
 
+/** Providers that can be used in Discordoo */
 export enum DiscordooProviders {
   REST,
   GATEWAY,
   CACHE,
 }
 
+/** Operation Codes used in Inter-process communication messages */
 export enum IpcOPCodes {
-  DISPATCH, // send/receive
-  IDENTIFY, // send
-  HEARTBEAT, // send/receive
-  INVALID_SESSION, // send
-  HELLO, // receive
-  REQUEST_STATS, // send/receive
-  ERROR, // send/receive
+  /** Includes common event (t) (send/receive) */
+  DISPATCH,
+  /** IPC Server should send this to identify itself in IPC Client (send) */
+  IDENTIFY,
+  /** IPC Server / IPC Client sends this to each other to identify that they are alive (send/receive) */
+  HEARTBEAT,
+  /** IPC Server sends this to IPC Client when IPC Client id (ddoo snowflake) is invalid (send) */
+  INVALID_SESSION,
+  /** IPC Client sends this to IPC Server to explain to him who he is. Contains shards list, heartbeat interval (receive) */
+  HELLO,
+  /** Used to statistics requests (send/receive) */
+  REQUEST_STATS,
+  /** Looks like something went wrong (send/receive) */
+  ERROR,
 }
 
+/** Inter-process communication events (t) */
 export enum IpcEvents {
+  /** Any messages between IPC Server and IPC Client */
   MESSAGE = 'MESSAGE',
-  DESTROYING = 'DESTROYING', // stop all shards
-  RESTRUCTURING = 'RESTRUCTURING', // set new shards configuration & restart (CROSS MACHINES ONLY)
-  RESTARTING = 'RESTARTING', // restart all shards
-  PARTIAL_RESTARTING = 'PARTIAL_RESTARTING', // restart specified shards
+  /** Stop all shards */
+  DESTROYING = 'DESTROYING',
+  /** Set new shards configuration & restart (between machines only) */
+  RESTRUCTURING = 'RESTRUCTURING',
+  /** Restart all shards */
+  RESTARTING = 'RESTARTING',
+  /** Restart specified shards */
+  PARTIAL_RESTARTING = 'PARTIAL_RESTARTING',
 }
 
+/** Sharding manager modes */
 export const enum ShardingModes {
+  /** Spawn sharding instances in different processes */
   PROCESSES = 'processes',
+  /** Spawn sharding instances in different worker threads */
   WORKERS = 'workers',
+  /** Spawn sharding instances in different node.js's clusters */
   CLUSTERS = 'clusters',
+  /** Connect to child sharding managers in different machines and pass sharding instructions to them */
   MACHINES = 'machines',
 }
 
+/* Child sharding manager modes (used on a remote machines when 'machines' sharding mode is used) **/
 export enum PartialShardingModes {
+  /** Spawn sharding instances in different processes */
   PROCESSES = 'processes',
+  /** Spawn sharding instances in different worker threads */
   WORKERS = 'workers',
+  /** Spawn sharding instances in different node.js's clusters */
   CLUSTERS = 'clusters',
 }
 
--- src/core/ShardListResolvable.ts
+++ src/core/ShardListResolvable.ts
@@ -1,2 +1,2 @@
+/** Represents a shards list that can be resolved to array of shards ids */
 export type ShardListResolvable = number | number[] | { from: number; to: number }
-
