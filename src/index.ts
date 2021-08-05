bd8dd2cec92dc930766e4f10a31a36045edbde27
Mirdukkk <vdvmirduk@gmail.com>
2021-08-06 02:00:02 +0300

    fix: add tslib to dependencies
    chore: use default export

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,36 @@
-export * from './cache'
-export * from './collection'
-export * from './constants'
-export * from './core'
-export * from './entities'
-export * from './gateway'
-export * from './rest'
-export * from './sharding'
-export * from './utils'
-export * from './wrapper'
+import * as Cache from './cache'
+import * as Collection from './collection'
+import * as Constants from './constants'
+import * as Core from './core'
+import * as Entities from './entities'
+import * as Gateway from './gateway'
+import * as Rest from './rest'
+import * as Sharding from './sharding'
+import * as Utils from './utils'
+import * as Wrapper from './wrapper'
+
+export {
+  Cache,
+  Collection,
+  Constants,
+  Core,
+  Entities,
+  Gateway,
+  Rest,
+  Sharding,
+  Utils,
+  Wrapper,
+}
+
+export default {
+  Cache,
+  Collection,
+  Constants,
+  Core,
+  Entities,
+  Gateway,
+  Rest,
+  Sharding,
+  Utils,
+  Wrapper,
+}
