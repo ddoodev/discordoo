903812b9a9227147bda38f1e7496070cf9e471ff
Mirdukkk <vdvmirduk@gmail.com>
2021-08-06 04:34:32 +0300

    feat(rest): export rest

--- src/rest/index.ts
+++ src/rest/index.ts
@@ -1,4 +1,21 @@
+import { RestManagerRequestOptions } from '@src/rest/interfaces/RestManagerRequestOptions'
+import { RestManagerRequestData } from '@src/rest/interfaces/RestManagerRequestData'
 import { DefaultRestProvider } from '@src/rest/DefaultRestProvider'
+import { RawAttachment } from '@src/rest/interfaces/RawAttachment'
+import { RestManagerOptions } from '@src/rest/RestManagerOptions'
+import { RestLimitsManager } from '@src/rest/RestLimitsManager'
+import { RestOptions } from '@src/rest/interfaces/RestOptions'
+import { makeRequest } from '@src/rest/makeRequest'
+import { RestManager } from '@src/rest/RestManager'
+
 export {
-  DefaultRestProvider
+  RestManagerRequestOptions,
+  RestManagerRequestData,
+  DefaultRestProvider,
+  RawAttachment,
+  RestManagerOptions,
+  RestLimitsManager,
+  RestOptions,
+  makeRequest,
+  RestManager,
 }
