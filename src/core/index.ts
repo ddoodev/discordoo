c381f7d805cf20f8841aa9a456780b121def1507
Kakarot <kakarot.joel@gmail.com>
2021-08-18 17:42:37 +0530

    Core imports

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,24 +1,12 @@
-import { GatewayConnectOptions } from '@src/core/providers/gateway/options/GatewayConnectOptions'
-import { ProviderConstructor } from '@src/core/providers/ProviderConstructor'
-import { GatewayProvider } from '@src/core/providers/gateway/GatewayProvider'
-import { ClientEventsHandlers } from '@src/core/client/ClientEventsHandlers'
-import { DefaultClientStack } from '@src/core/client/DefaultClientStack'
-import { CacheProvider } from '@src/core/providers/cache/CacheProvider'
-import { ProviderOption } from '@src/core/providers/ProviderOption'
-import { ClientInternals } from '@src/core/client/ClientInternals'
-import { ClientOptions } from '@src/core/client/ClientOptions'
+export { GatewayConnectOptions } from '@src/core/providers/gateway/options/GatewayConnectOptions'
+export { ProviderConstructor } from '@src/core/providers/ProviderConstructor'
+export { GatewayProvider } from '@src/core/providers/gateway/GatewayProvider'
+export { ClientEventsHandlers } from '@src/core/client/ClientEventsHandlers'
+export { DefaultClientStack } from '@src/core/client/DefaultClientStack'
+export { CacheProvider } from '@src/core/providers/cache/CacheProvider'
+export { ProviderOption } from '@src/core/providers/ProviderOption'
+export { ClientInternals } from '@src/core/client/ClientInternals'
+export { ClientOptions } from '@src/core/client/ClientOptions'
 
-import { Client } from '@src/core/client/Client'
+export { Client } from '@src/core/client/Client'
 
-export {
-  GatewayConnectOptions,
-  ProviderConstructor,
-  GatewayProvider,
-  ClientEventsHandlers,
-  DefaultClientStack,
-  CacheProvider,
-  ProviderOption,
-  ClientInternals,
-  ClientOptions,
-  Client,
-}
