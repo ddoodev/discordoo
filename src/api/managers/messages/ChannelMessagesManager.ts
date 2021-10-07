554c8b7eb34f17fda92bde9147367313b53ad902
Mirdukkk <vdvmirduk@gmail.com>
2021-10-08 02:44:31 +0300

    fix: fix build

---
+++ src/api/managers/messages/ChannelMessagesManager.ts
@@ -0,0 +1,30 @@
+import { EntitiesCacheManager, EntitiesManager, Message } from '@src/api'
+import { Client } from '@src/core'
+import { MessagesManagerData } from '@src/api/managers/interfaces/MessagesManagerData'
+import { AbstractChannel } from '@src/api/entities/channel/AbstractChannel'
+import { MessageContent } from '@src/api/entities/message/interfaces/MessageContent'
+import { SendOptions } from '@src/api/entities/message/interfaces/SendOptions'
+
+export class ChannelMessagesManager extends EntitiesManager {
+  public cache: EntitiesCacheManager<Message>
+  public channel?: AbstractChannel
+  public channelId: string
+
+  constructor(client: Client, data: MessagesManagerData) {
+    super(client)
+
+    this.channel = data.channel
+    this.channelId = data.channelId
+
+    this.cache = new EntitiesCacheManager<Message>(this.client, {
+      keyspace: 'messages',
+      storage: this.channelId,
+      entity: 'Message',
+      policy: 'messages'
+    })
+  }
+
+  async create(content: MessageContent, options?: SendOptions) {
+    return this.client.messages.create(this.channelId, content, options)
+  }
+}
