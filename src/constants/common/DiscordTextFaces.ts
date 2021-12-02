b4e358b1c4f856ef4d39ce24e3ec9e8081cba862
trip <67926726+san4ern@users.noreply.github.com>
2021-12-02 13:57:50 +0200

    Fixed grammar

--- src/constants/common/DiscordTextFaces.ts
+++ src/constants/common/DiscordTextFaces.ts
@@ -1,6 +1,6 @@
 /** Discord text faces, which provided by in-built slash commands */
 export enum DiscordTextFaces {
   Shrug = '¯\\_(ツ)\\_/¯',
-  Tabelflip = '(╯°□°）╯︵ ┻━┻',
+  Tableflip = '(╯°□°）╯︵ ┻━┻',
   Unflip = '┬─┬ ノ( ゜-゜ノ)',
 }
