2e17a9df298abebfbea6ba4cb7fedae662b842ed
trip <67926726+san4ern@users.noreply.github.com>
2021-12-02 15:20:41 +0200

    feat(Constants): add discord faces (#31)
    * feat(Constants): add discord faces 
    
    I've added discord faces from in-built slash commands
    
    * Update and rename DiscordFaces.ts to DiscordTextFaces.ts
    
    * Fixed grammar
    
    * Update constants/common/index.ts
    
    * chore: sort import
    
    Co-authored-by: Mirdukkk <44965055+Mirdukkk@users.noreply.github.com>

---
+++ src/constants/common/DiscordTextFaces.ts
@@ -0,0 +1,6 @@
+/** Discord text faces, which provided by in-built slash commands */
+export enum DiscordTextFaces {
+  Shrug = '¯\\_(ツ)\\_/¯',
+  Tableflip = '(╯°□°）╯︵ ┻━┻',
+  Unflip = '┬─┬ ノ( ゜-゜ノ)',
+}
--- src/constants/common/index.ts
+++ src/constants/common/index.ts
@@ -3,6 +3,7 @@ export { ToJsonOverrideSymbol } from '@src/constants/common/ToJsonOverrideSymbol
 export { DiscordooProviders } from '@src/constants/common/DiscordooProviders'
 export { SPOTIFY_CDN_IMAGES } from '@src/constants/common/SpotifyCdnImages'
 export { VideoQualityModes } from '@src/constants/common/VideoQualityModes'
+export { DiscordTextFaces } from '@src/constants/common/DiscordTextFaces'
 export { DISCORDOO_EPOCH } from '@src/constants/common/DiscordooEpoch'
 export { BIG_INT_POINTER } from '@src/constants/common/BigIntPointer'
 export { SPOILER_PREFIX } from '@src/constants/common/SpoilerPrefix'
