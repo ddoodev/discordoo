105b1f3532bd611bf6317e49127bd2511f52a4b4
trip <67926726+san4ern@users.noreply.github.com>
2021-12-02 11:06:39 +0200

    Update and rename DiscordFaces.ts to DiscordTextFaces.ts

--- src/constants/common/DiscordTextFaces.ts
+++ src/constants/common/DiscordTextFaces.ts
@@ -1,4 +1,5 @@
-export enum DiscordFaces {
+/** Discord text faces, which provided by in-built slash commands */
+export enum DiscordTextFaces {
   Shrug = '¯\\_(ツ)\\_/¯',
   Tabelflip = '(╯°□°）╯︵ ┻━┻',
   Unflip = '┬─┬ ノ( ゜-゜ノ)',
