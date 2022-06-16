e9fab679794e00623bdce96f35b215677113df84
Кислый Кислюк <kislball@yahoo.com>
2022-06-16 19:51:13 +0300

    feat(guild): add exports

--- src/api/entities/guild/index.ts
+++ src/api/entities/guild/index.ts
@@ -1,1 +1,6 @@
 export * from './interfaces'
+export { AbstractGuild } from '@src/api/entities/guild/AbstractGuild'
+export { AbstractViewableGuild } from '@src/api/entities/guild/AbstractViewableGuild'
+export { Guild } from '@src/api/entities/guild/Guild'
+export { InviteGuild } from '@src/api/entities/guild/InviteGuild'
+export { OAuth2Guild } from '@src/api/entities/guild/OAuth2Guild'
--- src/api/entities/guild/interfaces/index.ts
+++ src/api/entities/guild/interfaces/index.ts
@@ -1,1 +1,8 @@
+export { AbstractGuildData } from '@src/api/entities/guild/interfaces/AbstractGuildData'
 export { GuildResolvable } from '@src/api/entities/guild/interfaces/GuildResolvable'
+export { OAuth2GuildData } from '@src/api/entities/guild/interfaces/OAuth2GuildData'
+export { RawOAuth2GuildData } from '@src/api/entities/guild/interfaces/RawOAuth2GuildData'
+export { RawViewableGuildData } from '@src/api/entities/guild/interfaces/RawViewableGuildData'
+export { UnavailableGuildData } from '@src/api/entities/guild/interfaces/UnavailableGuildData'
+export { ViewableGuild } from '@src/api/entities/guild/interfaces/ViewableGuild'
+export { ViewableGuildData } from '@src/api/entities/guild/interfaces/ViewableGuildData'
