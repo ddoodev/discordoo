84359f5e05f6da8ef08c30afa7734310e418a612
mirdukkk <vdvmirduk@gmail.com>
2023-03-25 01:17:36 +0300

    fix: remove strange incomplete exports

--- src/api/entities/embed/index.ts
+++ src/api/entities/embed/index.ts
@@ -1,4 +1,4 @@
-export { MessageEmbedBuilder } from 'src/api/entities/embed/MessageEmbedBuilder'
+export { MessageEmbedBuilder } from './MessageEmbedBuilder'
 export { MessageEmbed } from './MessageEmbed'
 
 export * from './interfaces'
