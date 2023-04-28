f12a8e764dafa6a498e4c77dcd676c3cd549d2d3
mirdukkk <vdvmirduk@gmail.com>
2023-04-28 17:52:53 +0300

    docs: remove internal gateway functions

--- src/gateway/index.ts
+++ src/gateway/index.ts
@@ -3,5 +3,6 @@ export { WebSocketManager } from './WebSocketManager'
 export { WebSocketClient } from './WebSocketClient'
 export { GatewayManager } from './GatewayManager'
 
+// @ddocs ignore
 export * from './app'
 export * from './interfaces'
