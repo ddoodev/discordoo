39ca8a28ab7ccc33b9df676c67c59f06225ee8a5
BuildTools <48864283+hibanasama@users.noreply.github.com>
2023-07-16 17:10:55 +0500

    fix: component interaction can reply with modal

--- src/api/entities/interaction/interactions/WritableComponentInteraction.ts
+++ src/api/entities/interaction/interactions/WritableComponentInteraction.ts
@@ -1,7 +1,7 @@
-import { WritableInteraction } from '@src/api/entities/interaction/interactions/WritableInteraction'
+import { WritableModalInteraction } from '@src/api/entities/interaction/interactions/WritableModalInteraction'
 import { InteractionMessageContent, InteractionMessageEditOptions } from '@src/api'
 
-export abstract class WritableComponentInteraction extends WritableInteraction {
+export abstract class WritableComponentInteraction extends WritableModalInteraction {
   async editComponentReply(
     content: InteractionMessageContent, options?: Omit<InteractionMessageEditOptions, 'message'>
   ): Promise<boolean> {
