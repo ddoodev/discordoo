bec6a1a8f7b4385d889945839320a76e9f9ddcfc
HibanaSama <48864283+hibanasama@users.noreply.github.com>
2023-07-17 17:49:12 +0500

    fix: add missing fields in app command option builder

--- src/api/entities/interaction/builders/AppCommandOptionBuilder.ts
+++ src/api/entities/interaction/builders/AppCommandOptionBuilder.ts
@@ -1,4 +1,4 @@
-import { AppCommandOptionTypes, DiscordLocale } from '@src/constants'
+import { AppCommandOptionTypes, ChannelTypes, DiscordLocale } from '@src/constants'
 import {
   AppCommandOption,
   AppCommandOptionChoiceData,
@@ -19,6 +19,11 @@ export class AppCommandOptionBuilder {
   declare required: boolean
   declare value: string | number
   declare type: AppCommandOptionTypes
+  public minLength?: number
+  public maxLength?: number
+  public minValue?: number
+  public maxValue?: number
+  public channelTypes?: ChannelTypes[]
 
   constructor(
     data?: AppCommandOptionWithSubcommandsData
@@ -47,37 +52,11 @@ export class AppCommandOptionBuilder {
     }
   }
 
-  setName(name: string) {
-    this.name = name
-    return this
-  }
-
-  setNameLocalizations(nameLocalizations: Record<DiscordLocale, string>) {
-    this.nameLocalizations = nameLocalizations
-    return this
-  }
-
-  setDescription(description: string) {
-    this.description = description
-    return this
-  }
-
-  setDescriptionLocalizations(descriptionLocalizations: Record<DiscordLocale, string>) {
-    this.descriptionLocalizations = descriptionLocalizations
-    return this
-  }
-
-  addChoices(choices: Array<AppCommandOptionChoiceData | RawAppCommandOptionChoiceData>) {
-    choices.forEach(this.addChoice)
-    return this
-  }
-
   addChoice(choice: AppCommandOptionChoiceData | RawAppCommandOptionChoiceData) {
     let nameLocalizations: Record<DiscordLocale, string> | undefined = undefined
     if ('name_localizations' in choice) {
       nameLocalizations = choice.name_localizations
-    }
-    else if ('nameLocalizations' in choice) {
+    } else if ('nameLocalizations' in choice) {
       nameLocalizations = choice.nameLocalizations
     }
 
@@ -88,6 +67,11 @@ export class AppCommandOptionBuilder {
     }
 
     this.choices.push(rawChoiceData)
+    return this
+  }
+
+  addChoices(choices: Array<AppCommandOptionChoiceData | RawAppCommandOptionChoiceData>) {
+    choices.forEach(this.addChoice)
     return this
   }
 
@@ -104,18 +88,63 @@ export class AppCommandOptionBuilder {
     return this
   }
 
+  setChannelTypes(channelTypes: ChannelTypes[]) {
+    this.channelTypes = channelTypes
+    return this
+  }
+
+  setDescription(description: string) {
+    this.description = description
+    return this
+  }
+
+  setDescriptionLocalizations(descriptionLocalizations: Record<DiscordLocale, string>) {
+    this.descriptionLocalizations = descriptionLocalizations
+    return this
+  }
+
+  setMaxLength(value: number) {
+    this.maxLength = value
+    return this
+  }
+
+  setMaxValue(value: number) {
+    this.maxValue = value
+    return this
+  }
+
+  setMinLength(value: number) {
+    this.minLength = value
+    return this
+  }
+
+  setMinValue(value: number) {
+    this.minValue = value
+    return this
+  }
+
+  setName(name: string) {
+    this.name = name
+    return this
+  }
+
+  setNameLocalizations(nameLocalizations: Record<DiscordLocale, string>) {
+    this.nameLocalizations = nameLocalizations
+    return this
+  }
+
   setRequired(required: boolean) {
     this.required = required
     return this
   }
 
-  setValue(value: string | number) {
-    this.value = value
+  setType(type: AppCommandOptionTypes) {
+    this.type = type
     return this
   }
 
-  setType(type: AppCommandOptionTypes) {
-    this.type = type
+  setValue(value: string | number) {
+    this.value = value
     return this
   }
 
