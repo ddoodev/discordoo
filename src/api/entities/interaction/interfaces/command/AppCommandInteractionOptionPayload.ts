f0345511d7b71df656866d8e38ff63ea56a43ee4
HibanaSama <48864283+hibanasama@users.noreply.github.com>
2023-07-17 17:56:55 +0500

    fix: add missing type in app command interaction option payload

--- src/api/entities/interaction/interfaces/command/AppCommandInteractionOptionPayload.ts
+++ src/api/entities/interaction/interfaces/command/AppCommandInteractionOptionPayload.ts
@@ -6,7 +6,7 @@ export interface AppCommandInteractionOptionPayload {
   /** the type of option */
   type: AppCommandOptionTypes
   /** the value of the option resulting from user input */
-  value?: string | number
+  value?: string | number | boolean
   /** present if this option is a group or subcommand */
   options?: AppCommandInteractionOptionPayload[]
   /** `true` if this option is the currently focused option for autocomplete */
