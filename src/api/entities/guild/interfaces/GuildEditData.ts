0edc05a3d1b8711c1060315067e91c2374d2beef
Mirdukkk <44965055+Mirdukkk@users.noreply.github.com>
2023-08-18 17:11:53 +0300

    Update src/api/entities/guild/interfaces/GuildEditData.ts

--- src/api/entities/guild/interfaces/GuildEditData.ts
+++ src/api/entities/guild/interfaces/GuildEditData.ts
@@ -7,27 +7,27 @@ import {
 } from '@src/constants'
 
 export interface GuildEditData {
-  name?: string //
+  name?: string
   region?: string
-  icon?: BufferResolvable //
-  verificationLevel?: GuildVerificationLevels //
-  defaultNotifications?: GuildDefaultMessageNotificationLevel //
-  explicitContentFilter?: GuildExplicitContentFilterLevels //
-  afkChannelId?: string //
-  afkTimeout?: number //
-  systemChannelId?: string //
-  systemChannelFlags?: number //
-  rulesChannelId?: string //
+  icon?: BufferResolvable
+  verificationLevel?: GuildVerificationLevels
+  defaultNotifications?: GuildDefaultMessageNotificationLevel
+  explicitContentFilter?: GuildExplicitContentFilterLevels
+  afkChannelId?: string
+  afkTimeout?: number
+  systemChannelId?: string
+  systemChannelFlags?: number
+  rulesChannelId?: string
   publicUpdatesChannelId?: string
-  preferredLocale?: DiscordLocale //
-  features?: GuildFeatures[] //
-  description?: string //
-  banner?: BufferResolvable //
-  splash?: BufferResolvable //
+  preferredLocale?: DiscordLocale
+  features?: GuildFeatures[]
+  description?: string
+  banner?: BufferResolvable
+  splash?: BufferResolvable
   discoverySplash?: BufferResolvable
-  ownerId?: string //
+  ownerId?: string
   widgetEnabled?: boolean
   widgetChannelId?: string
-  premiumProgressEnabled?: boolean //
-  safetyAlertsChannelId?: string //
+  premiumProgressEnabled?: boolean
+  safetyAlertsChannelId?: string
 }
