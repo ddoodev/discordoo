dcf1a7755fd58dca6e42b10fca52a1f721bb71dd
HibanaSama <48864283+hibanasama@users.noreply.github.com>
2023-08-18 22:06:13 +0500

    fix: add missing exports

--- src/gateway/interfaces/index.ts
+++ src/gateway/interfaces/index.ts
@@ -1,5 +1,7 @@
 export { WebSocketClientDestroyOptions } from './WebSocketClientDestroyOptions'
 export { WebSocketClientEventsHandlers } from './WebSocketClientEventsHandlers'
+export { GatewayIdentifyProperties } from './GatewayIdentifyProperties'
+export { GatewayIntentsResolvable } from './GatewayIntentsResolvable'
 export { GatewayManagerData } from './GatewayManagerData'
 export { WebSocketPacket } from './WebSocketPacket'
 
