a3a6b90535587a4e4ca43182a98c14b93b855998
bit0r1n <me@bitor.in>
2024-02-20 14:47:49 +0300

    ref(events): create message category

--- src/events/ctx/index.ts
+++ src/events/ctx/index.ts
@@ -1,6 +1,6 @@
 export { GuildMembersChunkEventContext } from './GuildMembersChunkEventContext'
 export { PresenceUpdateEventContext } from './PresenceUpdateEventContext'
 export { ShardConnectedEventContext } from './ShardConnectedEventContext'
-export { MessageCreateEventContext } from './MessageCreateEventContext'
+export { MessageCreateEventContext } from '../message/ctx/MessageCreateEventContext'
 export { IpcMessageEventContext } from './IpcMessageEventContext'
 export { ReadyEventContext } from './ReadyEventContext'
